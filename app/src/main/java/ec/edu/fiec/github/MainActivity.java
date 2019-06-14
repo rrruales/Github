e2540537d3ce202aeae9d1e1cd03d2e31fc804d7
Estudiante <Estudiante@GGIECTEMWRK223.espol.edu.ec>
2019-06-14 11:27:45 -0500

    commit luis

--- app/src/main/java/ec/edu/fiec/github/MainActivity.java
+++ app/src/main/java/ec/edu/fiec/github/MainActivity.java
@@ -7,7 +7,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState);//cualquier cambip
         setContentView(R.layout.activity_main);
     }
 }
